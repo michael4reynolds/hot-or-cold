a828a082bfd4efa94ff8340db3f3e493753f8d16
Michael Reynolds <michael4reynolds@gmail.com>
2016-05-03 11:40:32 -0400

    ignore scratch files

---
+++ compiled/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+  /*--- Display information modal box ---*/
+  $(".what").click(function () {
+    $(".overlay").fadeIn(1000);
+  });
+
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function () {
+    $(".overlay").fadeOut(1000);
+  });
+});
+//# sourceMappingURL=app.js.map
